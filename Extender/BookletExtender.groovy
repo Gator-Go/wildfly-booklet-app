78796f7878e6a660e6d67a462fe946471c8d7240
Gator-Go <gator@sw-builder.com>
2026-07-10 12:00:30 -0400

    Initial commit: Booklet app from SocialBook / WildflyBuilder

---
+++ Extender/BookletExtender.groovy
@@ -0,0 +1,64 @@
+def changes = []
+
+public class MyData {
+
+  def theFile = ""
+
+  def extMarker =
+"""
+"""
+  def srcInsert =
+"""
+"""
+}
+MyData newData = new MyData()
+
+
+
+
+
+// ******** Done ********
+
+
+
+
+def dir = "../Booklet"
+
+def extFiles ( theDir, changes ) {
+
+   def fileList = new File(theDir).list().toList()
+
+   for ( i in fileList ) {
+
+      def inFile = theDir + "/" + i
+      def f1= new File(inFile)
+
+      MyData myData = new MyData();
+
+      if ( f1.isDirectory() ) {
+         extFiles ( inFile, changes )
+      } else {
+//println(i)
+        for (c in changes) {
+          MyData theData = c
+          if ( i.equals(theData.theFile) ) {
+            def oldFile = new File(inFile).text
+            def newMarker = theData.extMarker.replaceAll( "\\\n", "\\\r\\\n" )
+            def newSrc = theData.srcInsert.replaceAll( "\\\n", "\\\r\\\n" )
+            def newFile = oldFile.replace(newMarker, newSrc)
+            new File(inFile).write(newFile)
+            if (newFile.contains(newSrc) == false) { println(theData.theFile + " missing changes") }
+          }
+
+        }
+
+      } 
+   }
+}
+
+extFiles ( dir, changes )
+
+def src = new File("../booklet_logo.png").newDataInputStream()
+def dst = new File("../Booklet/booklet-war/src/main/webapp/resources/gfx/logo.png").newDataOutputStream()
+dst << src
+
